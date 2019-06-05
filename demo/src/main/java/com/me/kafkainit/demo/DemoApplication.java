e4afd8534666cd341e39a6d81fcead5b653f556e
Juan <juan.bofill10@gmail.com>
2019-06-05 16:44:02 -0400

    Remove unnecessary imports

--- demo/src/main/java/com/me/kafkainit/demo/DemoApplication.java
+++ demo/src/main/java/com/me/kafkainit/demo/DemoApplication.java
@@ -2,8 +2,7 @@ package com.me.kafkainit.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @SpringBootApplication
 public class DemoApplication {
